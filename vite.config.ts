4861fe4edcb4135e96518d12fd7411f8bb14799f
jfsteacher <36115178+jfsteacher@users.noreply.github.com>
2025-12-14 11:21:04 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/faq/',   // https://jfsteacher.github.io/faq/
-
-    exclude: ['lucide-react'],
-  },
+  base: '/faq/',
+  plugins: [react()],
 });
